6239218fd2a51c93400d4bb0a37a1fa3264a901f
maqsud <works@gmail.com>
2022-04-19 19:38:13 +0500

    works.js.2

---
+++ sss.js
@@ -0,0 +1,178 @@
+// let ism = prompt('ismingiz nima')
+
+// if (ism == 'proweb'){
+//     alert('salom boss')
+// } 
+// else{
+//     alert('salom '+ ism)
+// }
+// let yosh = prompt('yoshingiz nechida')
+
+// if (yosh<16){
+//     alert('siz hali yoshsiz')
+// } else if (yosh<60){
+//     alert('siz hali islashingiz kere')
+// }
+
+// let son =prompt('son kiriting') 
+// let aniq= son % 2
+// if (aniq==0){
+//     alert('juft')
+// } else if(aniq == 1){
+//     alert('toq son')
+// }
+// console.log(aniq);
+
+
+// let i = 1
+
+// while (i < 10){
+//     i++
+//     console.log(i);
+// } 
+
+// do{
+//     son = prompt('son kiriting')
+// }while (son == 0); 
+
+// son= prompt('son kiriting')
+// // console.log(son.length);
+// let son2 =2
+// for (let i = 0; i <= son; i++) {
+//      son2= son2 + 2  
+//      console.log(son2);
+// }
+
+// let son = prompt('son kiriting')
+// let javob = 1
+// for (let i = 1; i <= son; i++){
+//     javob = javob *i
+//     console.log(javob);
+// } 
+    
+     let son1 = +prompt('1-sonni kiriting')
+     let son2 = +prompt('2-sonni kiriting')
+     let son3 = +prompt('3-sonni kiriting')
+     
+     
+if (son1 < son2 && son1 > son3) {
+    alert('o`rta son' + son1)
+} else if (son1 > son2 && son1 < son3){
+    alert('o`rata son' + son1)
+} else if (son2 < son1 && son2 > son3){
+    alert('o`rata son' + son2)
+} else if (son2 > son1 && son2 < son3){
+    alert('o`rata son' + son2)
+} else if (son3 < son1 && son3 > son2){
+    alert('o`rata son' + son3)
+} else if (son3 > son1 && son3 < son2){
+    alert('o`rata son' + son3)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
